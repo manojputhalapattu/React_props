0689ea4adc1bd889b734c5cfb46079765d8e2406
manoj puthalapattu <58524654+manojputhalapattu@users.noreply.github.com>
2020-07-06 12:46:38 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Card(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <img src={props.img} alt="" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <div>
+    <Card
+      name="beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="1234556"
+      email="beyonce@email.com"
+    />
+    <Card
+      name="Jack bauer"
+      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      tel="63789837"
+      email="beauer@gamil.com"
+    />
+    <Card
+name="Chuck norries"
+img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+tel="83745790"
+email="chuck@email.com"
+/>
+    {/* <h1>My Contacts</h1>
+
+    <h2>Beyonce</h2>
+    <img
+      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      alt="avatar_img"
+    />
+    <p>+123 456 789</p>
+    <p>b@beyonce.com</p>
+
+    <h2>Jack Bauer</h2>
+    <img
+      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      alt="avatar_img"
+    />
+    <p>+987 654 321</p>
+    <p>jack@nowhere.com</p>
+
+    <h2>Chuck Norris</h2>
+    <img
+      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+      alt="avatar_img"
+    />
+    <p>+918 372 574</p>
+    <p>gmail@chucknorris.com</p> */}
+  </div>,
+  document.getElementById("root")
+);
